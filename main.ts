b230e9d7552188de2b688415aea17d62f49c591b
mcasta13 <116083045+mcasta13@users.noreply.github.com>
2022-10-21 19:17:08 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,42 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index_y = 0; index_y <= 4; index_y++) {
+        for (let index_x = 0; index_x <= 4; index_x++) {
+            led.unplot(index_x, index_y)
+        }
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 5; index++) {
+        for (let index_y = 0; index_y <= 4; index_y++) {
+            for (let index_x = 0; index_x <= 4; index_x++) {
+                led.toggle(index_x, index_y)
+            }
+        }
+        basic.pause(200)
+    }
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index_y = 0; index_y <= 4; index_y++) {
+        for (let index_x = 0; index_x <= 4; index_x++) {
+            led.unplot(index_x, Math.map(index_y, 0, 4, 4, 0))
+        }
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 5; index++) {
+        for (let index_y = 0; index_y <= 4; index_y++) {
+            for (let index_x = 0; index_x <= 4; index_x++) {
+                led.toggle(index_x, index_y)
+            }
+        }
+        basic.pause(200)
+    }
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+for (let index_y = 0; index_y <= 4; index_y++) {
+    for (let index_x = 0; index_x <= 4; index_x++) {
+        led.plot(index_x, index_y)
+    }
+}
+basic.forever(function () {
+	
+})
